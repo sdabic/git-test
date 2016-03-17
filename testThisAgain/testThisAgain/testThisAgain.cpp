7e507744baa58f9063e57112d370ae4dc6f09ad8
sdabic <srmdabic@gmail.com>
2016-03-17 15:57:22 -0700

    First run & build!

--- testThisAgain/testThisAgain/testThisAgain.cpp
+++ testThisAgain/testThisAgain/testThisAgain.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cout << "HELLO WORLD!" << endl;
+
+	int goose = 0;
+
+	cin >> goose;
+
 	return 0;
 }
 
